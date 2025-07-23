543a3e09dce120e09592d598c7d76966557d2b3d
Dylan DiBeneditto <dibenedittod@gmail.com>
2025-07-23 14:27:53 -0400

    remove gitignore files

--- index.py
+++
@@ -1,8 +0,0 @@
-from cmov import *
-from cmov.components import *
-
-if __name__ == "__main__":
-    scene = Scene(800,800, fps=100)
-    text = [Text("Hola world lalalalalala", x=100, y=i*42+100, size=32, color="#ffffff") for i in range(10)]
-    scene.play(CompositeAnimation.stagger(*[t.fadein() for t in text], stagger="0.1s"), "1s")
-    scene.render()
