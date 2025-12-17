39b6cb2b9e6eb8ca14e938ae9c4a59c2d80ceec5
Dylan DiBeneditto <dibenedittod@gmail.com>
2025-12-16 21:38:50 -0500

    fixed init to include all to allow for star import

--- src/cmov/components/__init__.py
+++ src/cmov/components/__init__.py
@@ -2,3 +2,5 @@ from .box import Box
 from .text import Text
 from .align import Align
 from .align import get_aligned_position
+
+__all__ = ['Box', 'Text', 'Align', 'get_aligned_position']
--- src/cmov/core/__init__.py
+++ src/cmov/core/__init__.py
@@ -2,3 +2,5 @@ from .animation import Animation
 from .component import Component
 from .composite_anim import CompositeAnimation
 from .scene import Scene
+
+__all__ = ['Animation', 'Component', 'CompositeAnimation', 'Scene']
